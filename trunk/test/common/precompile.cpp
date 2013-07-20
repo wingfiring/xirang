3671a1cf7836a3bc2a021782b9f68b652c2e6375
Sun, Chaoyang <scy@icerote.com>
2013-07-20 10:49:37 +0800

    add post build in ut

--- trunk/test/common/precompile.cpp
+++ trunk/test/common/precompile.cpp
@@ -2,12 +2,11 @@
 $COMMON_HEAD_COMMENTS_CONTEXT$
 */
 
-//#define BOOST_TEST_MAIN
 #ifdef USE_BOOST_TEST_DYN_LINK
 #	define BOOST_TEST_DYN_LINK
+#	define BOOST_TEST_MAIN
 #endif
 
-#define BOOST_TEST_MAIN
 #define BOOST_TEST_MODULE darksunne_xirang
 
 #include "precompile.h"
